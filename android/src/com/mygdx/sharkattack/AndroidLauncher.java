63197b9fe7f7f2fe27ee6e9fe99c7713b698b5d4
Cameron Jensen <jensenseanc@gmail.com>
2017-06-19 08:11:50 -0700

    add initial files for game

---
+++ android/src/com/mygdx/sharkattack/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.sharkattack;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.sharkattack.Shark Attack;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Shark Attack(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/sharkattack/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.sharkattack.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.sharkattack.Shark Attack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shark Attack(), config);
+	}
+}

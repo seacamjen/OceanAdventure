eab84c1478e4995913d3ad91dceeb4be7eae1986
Cameron Jensen <jensenseanc@gmail.com>
2017-06-19 13:19:57 -0700

    map loaded and scroll right on click

---
+++ android/assets/mapstuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="NES - Super Mario Bros 3 - Background Elements" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1702" columns="37">
+ <image source="NES - Super Mario Bros 3 - Background Elements.gif" width="678" height="830"/>
+</tileset>
--- core/src/com/mygdx/sharkattack/screens/PlayScreen.java
+++ core/src/com/mygdx/sharkattack/screens/PlayScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.sharkattack.SharkAttack;
@@ -15,11 +18,20 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(SharkAttack game) {
         this.game = game;
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(SharkAttack.V_WIDTH, SharkAttack.V_HEIGHT, gamecam);
         hud = new Hud(game.batch);
+
+        maploader = new TmxMapLoader();
+        map = maploader.load("oceanfloor.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -27,10 +39,26 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt;
+
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
